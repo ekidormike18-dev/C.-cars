bb0357865ef426e78cfa5e18cb116251e4da8b88
ekidormike18-dev <ekidormike18@gmail.com>
2026-03-10 11:38:54 +0300

    add document

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.querySelector('.menu').addEventListener('click',()=>{
         item.classList.toggle("change");
     });
 });
+
+document.querySelectorAll(".wrapper").forEach((item) =>{
+    item.addEventListener("click",()=>{
+        document.querySelectorAll(".target").forEach((item)=>{
+            item.classList.remove("change");
+        });
+    });
+});
