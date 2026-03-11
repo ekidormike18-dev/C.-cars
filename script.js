ae3d549b2852e1725252859d8f04e43fded4311d
ekidormike18-dev <ekidormike18@gmail.com>
2026-03-11 22:50:14 +0300

    section-3

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-document.querySelector('.menu').addEventListener('click',()=>{
-    document.querySelectorAll(".target").forEach((item) => {
-        item.classList.toggle("change");
-    });
+document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelectorAll('.target').forEach((item) => {
+    item.classList.toggle('change');
+  });
 });
 
-document.querySelectorAll(".wrapper").forEach((item) =>{
-    item.addEventListener("click",()=>{
-        document.querySelectorAll(".target").forEach((item)=>{
-            item.classList.remove("change");
-        });
+document.querySelectorAll('.wrapper').forEach((item) => {
+  item.addEventListener('click', () => {
+    document.querySelectorAll('.target').forEach((target) => {
+      target.classList.remove('change');
     });
+  });
 });
+
+// Video hover play/pause behavior
+const videos = document.querySelectorAll('.video');
+
+videos.forEach((video) => {
+  video.addEventListener('mouseover', () => {
+    video.play();
+  });
+
+  video.addEventListener('mouseout', () => {
+    video.pause();
+    video.currentTime = 0;
+  });
+});
+
